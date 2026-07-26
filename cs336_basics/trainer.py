997a3b6ef18b5159c80ba06973b6e08c5117f92e
konishdutta <konishdutta@gmail.com>
2026-07-26 09:37:07 -0700

    add ce loss

--- cs336_basics/trainer.py
+++ cs336_basics/trainer.py
@@ -7,12 +7,12 @@ def cross_entropy(preds, target):
      −log (exp(o_correct)) + log(sum(exp(o_i))) = log(sum(exp(o_i))) - o_correct
     """
     # preds are [... seq_len vocab_size]
-    # targets are [... seq_len]
+    # target are [... seq_len]
     seq_len = target.shape[-1]
     max_preds, _ = preds.max(dim=-1, keepdim=True)
     scaled_logits = preds - max_preds
 
-    correct = scaled_logits[..., torch.arange(seq_len), target[..., torch.arange(seq_len)]]
-    denom = scaled_logits.exp().sum(dim=-1).log()
+    correct = torch.gather(scaled_logits, dim=-1, index=target.unsqueeze(-1))
+    log_denom = scaled_logits.exp().sum(dim=-1).log()
 
-    return (denom - correct).mean()
+    return (log_denom - correct).mean()
